fe466217ea5a3ead448c5e523cff652b6943b6e5
MCHAITANYANAIDU <mchaitanyaprasad65@gmail.com>
2025-04-09 17:43:14 +0530

    everything done day1 to day2

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -1,26 +1,39 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Day1Assignments from './components/Day1/Day1Assignments';
+import Day2Assignments from './components/Day2/Day2Assignments';
+import Day3Assignments from './components/Day3/Day3Assignments';
+import Day4Assignments from './components/Day4/Day4Assignments';
+import FDCalculator from './components/Day3/FDCalculator';
+import RDSummary from './components/Day3/RDSummary';
+import InvestmentHistory from './components/Day3/InvestmentHistory';
+import InvestmentDetail from './components/Day3/InvestmentDetail';
+import Dashboard from './components/Day3/Dashboard';
+import CreateFD from './components/Day3/CreateFD';
+import CreateRD from './components/Day3/CreateRD';
+import Support from './components/Day3/Support';
 
-// Home and Day Assignment Pages
-import Home from './Components/Home';
-import Day1Assignments from './Components/Day1/Day1Assignments';
-import Day2Assignments from './Components/Day2/Day2Assignments';
-import Day3Assignments from './Components/Day3/Day3Assignments';
-import Day4Assignments from './Components/Day4/Day4Assignments';
-
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/day1" element={<Day1Assignments />} />
         <Route path="/day2" element={<Day2Assignments />} />
-        <Route path="/day3" element={<Day3Assignments />} />
+        <Route path="/day3/*" element={<Day3Assignments />}>
+          <Route path="fixed-deposit" element={<FDCalculator />} />
+          <Route path="recurring-deposit" element={<RDSummary />} />
+          <Route path="investment-history" element={<InvestmentHistory />} />
+          <Route path="investment/:id" element={<InvestmentDetail />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="create-fd" element={<CreateFD />} />
+          <Route path="create-rd" element={<CreateRD />} />
+          <Route path="support" element={<Support />} />
+        </Route>
         <Route path="/day4" element={<Day4Assignments />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
-};
+}
 
 export default App;
--- bank_app/src/Components/Day3/Day3Assignments.jsx
+++ bank_app/src/Components/Day3/Day3Assignments.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from '../Home';
-import FDCalculator from './FDCalculator';
-import RDSummary from './RDSummary';
-import InvestmentHistory from './InvestmentHistory';
-import InvestmentDetail from './InvestmentDetail';
-import Dashboard from './Dashboard';
-import CreateFD from './CreateFD';
-import CreateRD from './CreateRD';
-import Support from './Support';
+import { Outlet } from 'react-router-dom';
 
 const Day3Assignments = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/fixed-deposit" element={<FDCalculator />} />
-      <Route path="/recurring-deposit" element={<RDSummary />} />
-      <Route path="/investment-history" element={<InvestmentHistory />} />
-      <Route path="/investment/:id" element={<InvestmentDetail />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/create-fd" element={<CreateFD />} />
-      <Route path="/create-rd" element={<CreateRD />} />
-      <Route path="/support" element={<Support />} />
-    </Routes>
+    <div>
+      <h2>Day 3 Assignments</h2>
+      <Outlet />
+    </div>
   );
 };
 
